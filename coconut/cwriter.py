9fb0edf8329570b0242baa65ab53660f38963b82
David Malcolm <dmalcolm@redhat.com>
2014-04-04 12:42:51 -0400

    add simple CWriter class

---
+++ coconut/cwriter.py
@@ -0,0 +1,35 @@
+#   Copyright 2014 David Malcolm <dmalcolm@redhat.com>
+#   Copyright 2014 Red Hat, Inc.
+#
+#   This is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see
+#   <http://www.gnu.org/licenses/>.
+
+class CWriter:
+    def __init__(self, out):
+        self.out = out
+        self._indent = 0
+
+    def writeln(self, line=None):
+        if line:
+            self.out.write('%s%s\n'
+                           % ('    ' * self._indent,
+                              line))
+        else:
+            self.out.write('\n')
+
+    def indent(self):
+        self._indent += 1
+
+    def outdent(self):
+        self._indent -= 1
